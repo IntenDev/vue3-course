777f05352dbeb1354343328eecee90765012ee19
ilakosterin <ilya@in10.top>
2022-06-28 14:38:09 +0300

    New

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+
+<div class="app">
+  <PostForm @create="createPost"/>
+  <PostList :posts="posts"/>
+
+</div>
 </template>
 
+<script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+export default {
+  components: {
+    PostList, PostForm
+  },
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'Javascript', body: 'Описаниен поста 1'},
+        {id: 2, title: 'Javascript', body: 'Описаниен поста 2'},
+        {id: 3, title: 'Javascript', body: 'Описаниен поста 3'},
+      ]
+    }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
 }
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
